b78b7bdf91d121cd5baaddfd7e9754410d82e1de
Cassandra Graves <cmgraves@sfu.ca>
2020-04-10 12:28:48 -0700

    Fixed Nav, fixed Footer, edited text margins

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "nav_bar") {
+  if (x.className === "topnav") {
     x.className += " responsive";
   } else {
-    x.className = "nav_bar";
+    x.className = "topnav";
   }
 }
